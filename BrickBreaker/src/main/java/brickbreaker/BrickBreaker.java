9070b8ae19313f0324fb70dd840090b9fa3d4d52
Jake Kallioniemi <37346883+JakeKallioniemi@users.noreply.github.com>
2019-11-18 21:49:33 +0200

    small change to config

--- BrickBreaker/src/main/java/brickbreaker/BrickBreaker.java
+++ BrickBreaker/src/main/java/brickbreaker/BrickBreaker.java
@@ -11,7 +11,6 @@ import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
